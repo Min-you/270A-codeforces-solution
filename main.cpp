a458c75beb23bdb3e04a6bad18dc88d709509df5
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-14 20:27:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ typedef long long ll;
 void solve(){
     int a; 
     cin >> a;
+    //the robot can only move angle a thus to find the angle that the side of the polygon are join we take 180-a
+    //we know that 360 mod (180 - a) will give us 0 thus the following solution
     cout << (360 % (180-a)== 0 ? "YES" : "NO") << endl;
 }
 
